3fe8669d93d4424e3c05c256d431bc50e9c462bb
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-05-30 15:53:03 +0800

    updated location file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -34,14 +34,10 @@ ALLOWED_HOSTS = [
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-# Static files settings
+# Add these important settings:
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolioapp/static'),
-]
-
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolioapp/static')]
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -134,12 +130,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
