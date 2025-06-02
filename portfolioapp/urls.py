2ac5917c75615ac16d1d415b003020865e296e9f
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-02 11:06:30 +0800

    Nuclear static files fix

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import main, feedback
+from .views import main, feedback, static_test
 
 urlpatterns = [
     path('', main, name='main'),
     path('feedback/', feedback, name='feedback'),
+    path('static-test/', static_test, name='static-test'),
 ]
--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -6,3 +6,12 @@ def main(request):
 
 def feedback(request):
     return render(request, 'profile/feedback.html')
+
+# portfolioapp/views.py
+from django.http import FileResponse
+import os
+from django.conf import settings
+
+def static_test(request):
+    file_path = os.path.join(settings.STATIC_ROOT, 'vercel_final_test.txt')
+    return FileResponse(open(file_path, 'rb'))
