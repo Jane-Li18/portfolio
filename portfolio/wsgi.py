2a0f0ee226f9bd2dfe101576c2256cd033185c7d
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-02 11:30:25 +0800

    fixed?

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -4,5 +4,7 @@ from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-# Always use StaticFilesHandler in production
+# Both variables for maximum compatibility
 application = StaticFilesHandler(get_wsgi_application())
+app = application  # Alias for Vercel
+handler = application  # Another common name
