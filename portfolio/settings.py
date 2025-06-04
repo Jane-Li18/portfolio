83ae3507d281a5d5aed5af09f8e3a09395024a6b
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-04 18:03:27 +0800

    debug false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,10 +35,10 @@ ALLOWED_HOSTS = [
     'localhost'
 ]
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolioapp/static'),
 ]
