1fee3c04a7d670941a49f118aa4f9ef41b9abc5a
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-04 17:14:40 +0800

    uncorruppted background

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import main, feedback, static_test
+from .views import main, feedback
 
 urlpatterns = [
     path('', main, name='main'),
     path('feedback/', feedback, name='feedback'),
-    path('static-test/', static_test, name='static-test'),
 ]
--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -8,7 +8,3 @@ def main(request):
 
 def feedback(request):
     return render(request, 'profile/feedback.html')
-
-def static_test(request):
-    file_path = os.path.join(settings.STATIC_ROOT, 'vercel_final_test.txt')
-    return FileResponse(open(file_path, 'rb'))
