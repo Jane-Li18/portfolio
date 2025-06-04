bf0a738ab5d005aa8452c709226eddeb0447201b
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-04 17:57:18 +0800

    collectstatic new

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolioapp/static'),
 ]
