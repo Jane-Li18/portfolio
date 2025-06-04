b548639aad3967eb6f4168dd04aceddc5a026b3d
Jane-Li-18 <shanelee.jabonillo18@gmail.com>
2025-06-04 09:45:15 +0800

    staticfiles again

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,9 +40,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolioapp/static/assets'),
-    os.path.join(BASE_DIR, 'portfolioapp/static/css'),
-    os.path.join(BASE_DIR, 'portfolioapp/static/fonts'),
+    os.path.join(BASE_DIR, 'portfolioapp/static'),
 ]
 
 
